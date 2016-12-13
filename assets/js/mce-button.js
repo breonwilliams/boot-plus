4ceee2954e684beec48ba9a3b8d420e28db2cab6
breonw <breon_williams@unc.edu>
2016-12-12 21:39:08 -0500

    modify styles

--- assets/js/mce-button.js
+++ assets/js/mce-button.js
@@ -168,6 +168,12 @@
                             onclick: function() {
                                 editor.insertContent('[boot_logoutbtn linktext="Log Out" class="btn btn-primary"]');
                             }
+                        },
+                        {
+                            text: 'Password Reset',
+                            onclick: function() {
+                                editor.insertContent('[password_form]');
+                            }
                         }
                     ]
                 }
